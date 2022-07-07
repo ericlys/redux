62d37baeedc449d7f19d2c2d621a5d955483942f
Ericlys Moreira <ericlysm@gmail.com>
2022-07-07 16:57:15 -0300

    feat: redux typing

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,10 +1,11 @@
 import { useSelector } from "react-redux";
+import { IState } from "../store";
+import { ICartItem } from "../store/modules/cart/types";
 
 function Cart(){
 
-  const state = useSelector(state => state);
+  const cart = useSelector<IState, ICartItem[]>(state => state.cart.items);
 
-  console.log(state)
 
   return (
     <table>
@@ -17,9 +18,14 @@ function Cart(){
         </tr>
       </thead>
       <tbody>
-      {/* <tr>
-        <td></td> */}
-      {/* </tr> */}
+      {cart.map( item => (
+        <tr key={item.product.id}>
+          <td>{item.product.title}</td>
+          <td>{item.product.price}</td>
+          <td>{item.quantity}</td>
+          <td>{(item.product.price * item.quantity).toFixed(2)}</td>
+        </tr>
+      ))}
     </tbody>
     </table>
     
