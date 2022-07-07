21ee973194a85489b5cc2988ae60efd33879fc12
Ericlys Moreira <ericlysm@gmail.com>
2022-07-07 16:40:12 -0300

    feat: changing state

--- src/store/modules/cart/actions.ts
+++ src/store/modules/cart/actions.ts
@@ -2,7 +2,7 @@ import { IProduct } from "./types";
 
 export function addProductToCart(product: IProduct) {
   return {
-    type: 'ADD PRODUCT_TO_CART',
+    type: 'ADD_PRODUCT_TO_CART',
     payload: {
       product,
     }
