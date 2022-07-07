7e1f1cd910bb892d919f0b00cbdd5c882da700f7
Ericlys Moreira <ericlysm@gmail.com>
2022-07-07 15:55:41 -0300

    feat: add server-json

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,12 +1,29 @@
-import { useSelector, useStore } from "react-redux";
+import { useEffect, useState } from "react";
+import { IProduct } from "../store/modules/cart/types";
+import api from "../services/api";
 
 function Catalog(){
-  const catalog = useSelector(state => state);
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
 
-  console.log(catalog);
+  useEffect(() => {
+    api.get('products').then( response => {
+      setCatalog(response.data);
+    })
+  },[])
 
   return (
-    <h1>Catalog</h1>
+    <main>
+      <h1>Catalog</h1>
+
+      {catalog.map(product => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> { " - "}
+          <span>{product.price}</span> {" "}
+
+          <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
   );
 }
 
